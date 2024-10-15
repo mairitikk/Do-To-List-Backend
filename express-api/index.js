87676fa3e7a6ef29755f622f8a59460b5741b069
mairi tikk <mairi.tikk@gmail.com>
2024-10-15 10:48:38 +0200

    add body Parser

--- express-api/index.js
+++ express-api/index.js
@@ -1,15 +1,22 @@
-// Build a server with Node's HTTP module 
-import { createServer } from "http";
-const port = 3001;
-const server = createServer();
-
-server.on("request", (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.end("Hello, server!");
+// Import packages and set the port 
+import bodyParser from "body-parser";
+import express from "express";
+const port = 3002;
+const app = express();
+// Use Node.js body parsing middleware 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.get("/", (request, response) => {
+    response.send({
+        message: "Node.js and Express REST API",
+    });
 });
-
 // Start the server 
-server.listen(port, (error) => {
+const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server listening on port ${server.address().port}`);
 });
